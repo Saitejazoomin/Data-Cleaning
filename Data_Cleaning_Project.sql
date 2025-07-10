4063a79489adecb8de8eac2c8744a9b5bd2a43df
Sai teja <159143408+Saitejazoomin@users.noreply.github.com>
2025-07-10 16:40:43 +0530

    Data_Cleaning_ and EDA Using SQLProject.sql

--- Data_Cleaning_Project.sql
+++ Data_Cleaning_Project.sql
@@ -168,4 +168,87 @@ DROP COLUMN row_num;
 
 SELECT *
 FROM layoffs_staging2;
+
+
+
+-- Exploratory Data Analysis
+
+SELECT *
+FROM layoffs_staging2;
+
+SELECT max(total_laid_off) , MAX(percentage_laid_off)
+FROM layoffs_staging2;
+
+SELECT *
+FROM layoffs_staging2
+WHERE percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
+
+SELECT company,SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT MIN(`date`), MAX(`date`)
+FROM layoffs_staging2;
+
+SELECT industry,SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+SELECT country ,SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+SELECT YEAR(`date`) ,SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(`date`)
+ORDER BY 2 DESC;
+
+SELECT stage ,SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
+
+SELECT company,SUM(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT SUBSTRING(`date`,1,7) AS `Month`, SUM(total_laid_off)
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `Month`
+ORDER BY 1 ASC;
+
+WITH Rolling_total AS
+(SELECT SUBSTRING(`date`,1,7) AS `Month`, SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `Month`
+ORDER BY 1 ASC)
+SELECT `Month`,total_off, SUM(total_off) OVER(ORDER BY `Month`) AS rolling_total
+FROM Rolling_total;
+
+
+SELECT company, YEAR(`date`),SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC;
+
+WITH Company_year (Company, Years,total_laid_off) AS
+(SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC
+), Company_Year_Rank AS 
+ (SELECT *,
+DENSE_RANK () OVER( PARTITION BY Years ORDER BY total_laid_off DESC) AS Ranking
+FROM Company_year
+WHERE Years IS NOT NULL
+) SELECT *
+FROM Company_Year_Rank
+WHERE Ranking <= 5;
  
